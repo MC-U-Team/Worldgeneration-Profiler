eab55b4898821ee209a3efe85ce17682f8f08242
HyCraftHD <hycrafthd@live.de>
2020-02-23 16:40:26 +0100

    Works, but bad with script ...

--- src/main/java/info/u_team/world_generation_profiler/WorldGenerationProfilerMod.java
+++ src/main/java/info/u_team/world_generation_profiler/WorldGenerationProfilerMod.java
@@ -1,5 +1,16 @@
 package info.u_team.world_generation_profiler;
 
+import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.core.*;
+import org.apache.logging.log4j.core.Filter.Result;
+import org.apache.logging.log4j.core.appender.*;
+import org.apache.logging.log4j.core.appender.rolling.*;
+import org.apache.logging.log4j.core.config.*;
+import org.apache.logging.log4j.core.filter.*;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.layout.PatternLayout;
+import org.apache.logging.log4j.core.script.Script;
+
 import info.u_team.u_team_core.util.verify.JarSignVerifier;
 import info.u_team.world_generation_profiler.config.CommonConfig;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -11,8 +22,56 @@ public class WorldGenerationProfilerMod {
 	
 	public static final String MODID = "worldgenerationprofiler";
 	
+	public static final Logger LOGGER = (Logger) LogManager.getLogger("World Generation Profiler");
+	public static final Marker MARKER = MarkerManager.getMarker("World-Generation-Profiler");
+	
 	public WorldGenerationProfilerMod() {
 		JarSignVerifier.checkSigned(MODID);
 		ModLoadingContext.get().registerConfig(Type.COMMON, CommonConfig.CONFIG);
+		setupLogger();
 	}
+	
+	private void setupLogger() {
+		System.out.println("_____________________________________________XX");
+		LoggerContext context = LoggerContext.getContext(false);
+		Configuration configuration = context.getConfiguration();
+		
+		final String script = //
+				"var result = false \r\n" + //
+						"if (typeof logEvent === 'undefined') { \r\n" + //
+						"   result = false \r\n" + //
+						"} else { \r\n" + //
+						"   result = logEvent.getLoggerName().equals('" + LOGGER.getName() + "')\r\n" + //
+						"} \r\n" + //
+						"result;";
+		
+		configuration.addLoggerFilter((Logger) LogManager.getRootLogger(), ScriptFilter.createFilter(Script.createScript("script", "javascript", script), Result.DENY, Result.NEUTRAL, configuration));
+		
+		RollingRandomAccessFileAppender appender = RollingRandomAccessFileAppender.newBuilder() //
+				.setName("World Generation Profiler") //
+				.withFileName("logs/world-generation-profiler.log") //
+				.withFilePattern("logs/world-generation-profiler-%i.log.gz") //
+				.setLayout( //
+						PatternLayout.newBuilder() //
+								.withPattern("[%d{ddMMMyyyy HH:mm:ss.SSS}] [%t/%level] [%logger/%markerSimpleName]: %minecraftFormatting{%msg}{strip}%n%xEx").build() //
+				) //
+				.withPolicy(CompositeTriggeringPolicy.createPolicy( //
+						OnStartupTriggeringPolicy.createPolicy(1), //
+						SizeBasedTriggeringPolicy.createPolicy("200MB")))
+				.withStrategy( //
+						DefaultRolloverStrategy.newBuilder() //
+								.withMax("5") //
+								.withFileIndex("min") //
+								.build() //
+				).build();
+		appender.start();
+		
+		configuration.addLoggerAppender(LOGGER, appender);
+		
+		context.updateLoggers();
+		
+		LOGGER.info(MARKER, "YAAASDihzgdsiizjfhsdjkm,lfhsdkfksdjrhfgresdlukghzesruitlhjesdfuilhergjkldfhgjdflh");
+		LOGGER.info("GOGOGOGOOGOGOGOGOGOG,lfhsdkfksdjrhfgresdlukghzesruitlhjesdfuilhergjkldfhgjdflh");
+	}
+	
 }
--- src/main/java/info/u_team/world_generation_profiler/hook/StopWatchHook.java
+++ src/main/java/info/u_team/world_generation_profiler/hook/StopWatchHook.java
@@ -4,6 +4,16 @@ import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.core.*;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.Filter.Result;
+import org.apache.logging.log4j.core.LifeCycle.State;
+import org.apache.logging.log4j.core.appender.*;
+import org.apache.logging.log4j.core.appender.rolling.*;
+import org.apache.logging.log4j.core.config.*;
+import org.apache.logging.log4j.core.filter.MarkerFilter;
+import org.apache.logging.log4j.core.layout.PatternLayout;
+import org.apache.logging.log4j.message.Message;
 
 import com.google.common.base.Stopwatch;
 
@@ -12,11 +22,60 @@ import net.minecraft.world.chunk.*;
 
 public class StopWatchHook {
 	
-	private static final Logger LOGGER = LogManager.getLogger("World Generation Profiler");
+	private static final Logger LOGGER = (Logger) LogManager.getLogger("World Generation Profiler");
+	
+	/*static {
+		LoggerContext context = LoggerContext.getContext(false);
+		Configuration configuration = context.getConfiguration();
+		LoggerConfig loggerConfig = configuration.getLoggerConfig("World Generation Profiler");
+		
+		System.out.println("______________________________________________________________________________________");
+		// loggerConfig.getAppenders().forEach((s, a) -> {
+		// System.out.println(s + " - " + a);
+		// });
+		//
+		// loggerConfig.getAppenderRefs().forEach(a -> {
+		// System.out.println(a);
+		// });
+		
+		FileAppender a = ((FileAppender) loggerConfig.getAppenders().get("DebugFile"));
+		System.out.println(a.getFileName());
+		System.out.println(a.getManager());
+		
+//		context.addFilter(new Filter);
+		
+		if (loggerConfig.getName().equals("loggerName")) {
+			loggerConfig.removeAppender("appenderName");
+		} else {
+			throw new RuntimeException("There was no logger " + "loggerName");
+		}
+		context.updateLoggers();
+		
+		MarkerFilter.createFilter("yay", Result.ACCEPT, Result.DENY);
+		
+		RollingRandomAccessFileAppender.newBuilder() //
+				.setName("World Generation Profiler") //
+				.withFileName("logs/world-generation-profiler.log") //
+				.withFilePattern("logs/world-generation-profiler-%i.log.gz") //
+				.setLayout( //
+						PatternLayout.newBuilder() //
+								.withPattern("[%d{ddMMMyyyy HH:mm:ss.SSS}] [%t/%level] [%logger/%markerSimpleName]: %minecraftFormatting{%msg}{strip}%n%xEx").build() //
+				) //
+				.withPolicy(OnStartupTriggeringPolicy.createPolicy(1)) //
+				.withPolicy(SizeBasedTriggeringPolicy.createPolicy("200MB")) //
+				.withStrategy( //
+						DefaultRolloverStrategy.newBuilder() //
+								.withMax("5") //
+								.withFileIndex("min") //
+								.build() //
+				);
+				
+	}*/
 	
 	private static Map<ChunkStatus, Integer> THRESHOLD_MAP;
 	
 	public static void doGenerationWorkHook(ChunkStatus status, IChunk chunk, Stopwatch stopWatch) {
+		LOGGER.info("TEST YOLO");
 		if (THRESHOLD_MAP == null) {
 			THRESHOLD_MAP = new HashMap<ChunkStatus, Integer>();
 			CommonConfig.getInstance().getChunkStatusThresholdMap().forEach((chunkStatus, intValue) -> THRESHOLD_MAP.put(chunkStatus, intValue.get()));
