e883d0f6a36193790827660d947f0db0845a8d2b
HyCraftHD <hycrafthd@live.de>
2021-06-17 00:13:38 +0200

    Fixes #3 and HyCraftHD/Worldgeneration-Profiler#8

--- src/main/java/info/u_team/world_generation_profiler/hook/StopWatchHook.java
+++ src/main/java/info/u_team/world_generation_profiler/hook/StopWatchHook.java
@@ -10,6 +10,7 @@ import info.u_team.world_generation_profiler.WorldGenerationProfilerMod;
 import info.u_team.world_generation_profiler.config.CommonConfig;
 import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.chunk.IChunk;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class StopWatchHook {
 	
@@ -18,10 +19,19 @@ public class StopWatchHook {
 	public static void doGenerationWorkHook(ChunkStatus status, IChunk chunk, Stopwatch stopWatch) {
 		if (THRESHOLD_MAP == null) {
 			THRESHOLD_MAP = new HashMap<ChunkStatus, Integer>();
-			CommonConfig.getInstance().getChunkStatusThresholdMap().forEach((chunkStatus, intValue) -> THRESHOLD_MAP.put(chunkStatus, intValue.get()));
+			ForgeRegistries.CHUNK_STATUS.forEach(chunkStatus -> THRESHOLD_MAP.put(chunkStatus, 50)); // Add all currently registried chunk status entries with a default threshold of 50ms
+			CommonConfig.getInstance().getChunkStatusThresholdMap().forEach((chunkStatus, intValue) -> THRESHOLD_MAP.put(chunkStatus, intValue.get())); // Replace values with config values
 		}
+		
 		final long codeExecutionTime = stopWatch.elapsed(TimeUnit.MILLISECONDS);
-		if (THRESHOLD_MAP.get(status).intValue() <= codeExecutionTime) {
+		
+		final Integer threshold = THRESHOLD_MAP.get(status);
+		if (threshold == null) {
+			WorldGenerationProfilerMod.LOGGER.error("Chunkstatus " + status.getRegistryName() + " is not contained in the threshold config. Value not registered at the right time?");
+			return;
+		}
+		
+		if (threshold.intValue() <= codeExecutionTime) {
 			WorldGenerationProfilerMod.LOGGER.warn("Chunkstatus " + status.getRegistryName() + " took " + codeExecutionTime + " milliseconds to generate at chunk " + chunk.getPos());
 		}
 	}
